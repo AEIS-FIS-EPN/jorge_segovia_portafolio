aef35aa08b61360aa441a8f01bca7b4cf4838575
JorgeSegovia98 <jorge.segovia@epn.edu.ec>
2024-04-02 19:47:47 -0500

    feat: Add my photo and buttons that will lead to my information

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import fotoPerfil from './fotoPerfil.jpg'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-buttons">
+        <button className="button">Sobre mí</button>
+        <button className="button">Formación</button>
+        <button className="button">Proyectos</button>
+      </div>
+      <div className="App-content">
+        <img src={fotoPerfil} className="fotoPerfil" alt="Foto Jorge Segovia" />
+        <p className="App-intro">
+          Hola, mi nombre es Jorge Segovia, bienvenido a mi portafolio.
+        </p>
+      </div>
     </div>
   );
 }
